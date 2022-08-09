52a51c1a57537301942a147d1637112639a0afd2
Richard Saseun <richardsaseun@gmail.com>
2022-08-09 15:31:30 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'vascan-react-library'
+import 'vascan-react-library/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
